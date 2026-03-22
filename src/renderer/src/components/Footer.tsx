814bc271070ea207513bf47a6bc142a2147594e0
Chris Poulter <cpoulter@hotmail.co.uk>
2026-03-22 10:44:14 +0000

    fix: update footer text to remove "All rights reserved."

--- src/renderer/src/components/Footer.tsx
+++ src/renderer/src/components/Footer.tsx
@@ -5,8 +5,7 @@ export const Footer = (): React.JSX.Element => {
 
     return (
         <footer className="mt-auto bg-gray-800 p-4 text-center text-white">
-            &copy; {new Date().getFullYear()} Media Library. All rights
-            reserved.{' '}
+            &copy; {new Date().getFullYear()} Media Library{' '}
             {version && (
                 <span className="text-sm text-gray-400">v{version}</span>
             )}
